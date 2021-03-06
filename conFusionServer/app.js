812b687f388f1a76167e5a23a187ade9e085820c
gangulisubhajit <info.ganguli@gmail.com>
2021-03-07 01:47:07 +0530

    express session part-2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -41,39 +41,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log('req.session...', JSON.stringify(req.session));
 
   if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    console.log('authHeader...', authHeader);
-
-    if (!authHeader) {
-      var err = new Error('You ae not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var username = auth[0];
-    var password = auth[1];
-
-    if (username === 'admin' && password === 'password') {
-      req.session.user = 'admin';
-      next();
-    } else {
-      var err = new Error('You ae not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
+    var err = new Error('You ae not authenticated!');
+    err.status = 403;
+    return next(err);
   } else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       next();
     } else {
       var err = new Error('You ae not authenticated!');
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
@@ -83,8 +66,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
