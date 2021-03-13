6920c16868ffe701aeb9eef61550a1c7b843ee10
gangulisubhajit <info.ganguli@gmail.com>
2021-03-13 21:21:54 +0530

    oauth2.0 using facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '142050494470331',
+        clientSecret: '811bab0db9aa2a9b22b846ab61dc77e1'
+    }
 }
